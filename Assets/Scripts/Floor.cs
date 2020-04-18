0ea759ed52cbf4828d911046e53f5677182b2d0d
Tetrago <fpcjt@outlook.com>
2020-04-17 22:26:37 -0400

    Universal renderer and flooring.

---
+++ Assets/Scripts/Floor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Floor : MonoBehaviour
+{
+    public GameObject tile_;
+    public float range_;
+
+    private Dictionary<Vector3, GameObject> tiles_;
+
+    private void Update()
+    {
+        foreach(GameObject child in tiles_.Values)
+        {
+            if(Vector3.Distance(transform.position, child.transform.position) > range_)
+            {
+                Destroy(child);
+            }
+        }
+
+        for(float x = -range_; x < range_; ++x)
+        {
+            for(float y = -range_; y < range_; ++y)
+            {
+
+            }
+        }
+    }
+}
